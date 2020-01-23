9ee838fb9022442d4b0b8d006d1ab6f8e4ef245e
marlonanthony <mad1083@gmail.com>
2020-01-22 20:37:48 -0500

    prod

---
+++ static/precache-manifest.0fb7c7f15869114060bcf77e628a8492.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffbbacb5282ec135fe6f2bdc70f4a2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ba3bef34d59adbe2209",
+    "url": "/static/css/main.6e55a08c.chunk.css"
+  },
+  {
+    "revision": "6cd28b5fe3343b9c1991",
+    "url": "/static/js/2.011d259d.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.011d259d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ba3bef34d59adbe2209",
+    "url": "/static/js/main.326050dd.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "275a5a0f4e0bf9ad953a4c98399666bc",
+    "url": "/static/media/frontal.275a5a0f.jpg"
+  },
+  {
+    "revision": "b2ea42ec0c3786610af78b3bacf2ce78",
+    "url": "/static/media/gdup.b2ea42ec.jpg"
+  },
+  {
+    "revision": "bf42cb6997a799e50c6458d3454c5f92",
+    "url": "/static/media/skippys_logo.bf42cb69.jpg"
+  }
+]);
