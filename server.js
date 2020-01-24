16935f5f81924937861b2cea9a0fb80c845a518c
marlonanthony <mad1083@gmail.com>
2020-01-23 20:05:45 -0500

    prod

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(session({
 if(process.env.NODE_ENV === 'production') {
   app.use('/static', express.static(path.join(__dirname, '/static/static')))
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '/static/index.html'))
-  // })
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/static/index.html'))
+  })
 }
 
 const apollo = new ApolloServer({
