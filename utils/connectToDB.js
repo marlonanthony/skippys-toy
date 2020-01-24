f3d4bb0a6c359f49f4879c839473e9b20a1a186b
marlonanthony <mad1083@gmail.com>
2020-01-23 20:56:56 -0500

    prod

--- utils/connectToDB.js
+++ utils/connectToDB.js
@@ -4,8 +4,8 @@ module.exports = async db => {
   try {
     const mongo = await mongoose.connect(db, {
       useNewUrlParser: true, 
-      useCreateIndex: true,
-      useUnifiedTopology: true
+      // useCreateIndex: true,
+      // useUnifiedTopology: true
     })
     if(mongo) console.log('MongoDB connected')
   } catch(err){ throw err }
